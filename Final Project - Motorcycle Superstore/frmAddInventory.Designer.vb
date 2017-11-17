4f54b5d7619fd83183c0a9c548c4ceab48016302
LAPTOP-Q6FNUL87\skyda <skydaguy@gmail.com>
2017-11-17 18:06:10 -0500

    Finished Add Inventory form

--- Final Project - Motorcycle Superstore/frmAddInventory.Designer.vb
+++ Final Project - Motorcycle Superstore/frmAddInventory.Designer.vb
@@ -30,12 +30,21 @@ Partial Class frmAddInventory
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtYear = New System.Windows.Forms.TextBox()
+        Me.txtMake = New System.Windows.Forms.TextBox()
+        Me.txtModel = New System.Windows.Forms.TextBox()
+        Me.txtMilage = New System.Windows.Forms.TextBox()
+        Me.txtEngineSize = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(25, 28)
+        Me.Label1.Location = New System.Drawing.Point(25, 34)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(35, 13)
         Me.Label1.TabIndex = 0
@@ -44,7 +53,7 @@ Partial Class frmAddInventory
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(25, 54)
+        Me.Label2.Location = New System.Drawing.Point(25, 57)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(40, 13)
         Me.Label2.TabIndex = 1
@@ -53,7 +62,7 @@ Partial Class frmAddInventory
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(25, 82)
+        Me.Label3.Location = New System.Drawing.Point(25, 80)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(42, 13)
         Me.Label3.TabIndex = 2
@@ -62,7 +71,7 @@ Partial Class frmAddInventory
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(25, 106)
+        Me.Label4.Location = New System.Drawing.Point(25, 105)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(44, 13)
         Me.Label4.TabIndex = 3
@@ -98,17 +107,94 @@ Partial Class frmAddInventory
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(25, 197)
+        Me.Label8.Location = New System.Drawing.Point(25, 198)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(55, 13)
         Me.Label8.TabIndex = 7
         Me.Label8.Text = "Cylinders: "
         '
+        'txtYear
+        '
+        Me.txtYear.Location = New System.Drawing.Point(100, 31)
+        Me.txtYear.Name = "txtYear"
+        Me.txtYear.Size = New System.Drawing.Size(100, 20)
+        Me.txtYear.TabIndex = 8
+        '
+        'txtMake
+        '
+        Me.txtMake.Location = New System.Drawing.Point(100, 54)
+        Me.txtMake.Name = "txtMake"
+        Me.txtMake.Size = New System.Drawing.Size(100, 20)
+        Me.txtMake.TabIndex = 9
+        '
+        'txtModel
+        '
+        Me.txtModel.Location = New System.Drawing.Point(100, 77)
+        Me.txtModel.Name = "txtModel"
+        Me.txtModel.Size = New System.Drawing.Size(100, 20)
+        Me.txtModel.TabIndex = 10
+        '
+        'txtMilage
+        '
+        Me.txtMilage.Location = New System.Drawing.Point(100, 102)
+        Me.txtMilage.Name = "txtMilage"
+        Me.txtMilage.Size = New System.Drawing.Size(100, 20)
+        Me.txtMilage.TabIndex = 11
+        '
+        'txtEngineSize
+        '
+        Me.txtEngineSize.Location = New System.Drawing.Point(100, 172)
+        Me.txtEngineSize.Name = "txtEngineSize"
+        Me.txtEngineSize.Size = New System.Drawing.Size(100, 20)
+        Me.txtEngineSize.TabIndex = 14
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(100, 126)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(100, 21)
+        Me.ComboBox1.TabIndex = 15
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(100, 149)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(100, 21)
+        Me.ComboBox2.TabIndex = 16
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(100, 195)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(100, 21)
+        Me.ComboBox3.TabIndex = 17
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(58, 237)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(97, 23)
+        Me.btnAdd.TabIndex = 18
+        Me.btnAdd.Text = "Add to Inventory"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
         'frmAddInventory
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(535, 261)
+        Me.ClientSize = New System.Drawing.Size(235, 288)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.txtEngineSize)
+        Me.Controls.Add(Me.txtMilage)
+        Me.Controls.Add(Me.txtModel)
+        Me.Controls.Add(Me.txtMake)
+        Me.Controls.Add(Me.txtYear)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
@@ -132,4 +218,13 @@ Partial Class frmAddInventory
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
+    Friend WithEvents txtYear As TextBox
+    Friend WithEvents txtMake As TextBox
+    Friend WithEvents txtModel As TextBox
+    Friend WithEvents txtMilage As TextBox
+    Friend WithEvents txtEngineSize As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents btnAdd As Button
 End Class
