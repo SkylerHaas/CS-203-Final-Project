fd48512e0c862b38a52689f6e2a0a1f43e31a61b
LAPTOP-Q6FNUL87\skyda <skydaguy@gmail.com>
2017-11-25 17:35:15 -0500

    Cannot remember what I did in this commit. I think it had to do with selling inventory.

--- Final Project - Motorcycle Superstore/frmSellInventory.vb
+++ Final Project - Motorcycle Superstore/frmSellInventory.vb
@@ -1,5 +1,18 @@
 ﻿Public Class frmSellInventory
     Private Sub frmSellInventory_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        lblYear.Text = frmBrowseInventory.dgvInventory.SelectedCells.ToString
+        lblYear.Text = frmBrowseInventory.dgvInventory.SelectedCells(1).ToString
+        lblMake.Text = frmBrowseInventory.dgvInventory.SelectedCells(2).ToString
+        lblModel.Text = frmBrowseInventory.dgvInventory.SelectedCells(3).ToString
+        Dim price As Decimal = 0
+        Decimal.TryParse(frmBrowseInventory.dgvInventory.SelectedCells(9).ToString, price)
+        lblPrice.Text = price.ToString("c")
+    End Sub
+
+    Private Sub rdoLoan_CheckedChanged(sender As Object, e As EventArgs) Handles rdoLoan.CheckedChanged
+        If rdoLoan.Checked Then
+            grpLoanDetails.Enabled = True
+        Else
+            grpLoanDetails.Enabled = False
+        End If
     End Sub
 End Class
